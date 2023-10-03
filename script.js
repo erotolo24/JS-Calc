11cbf50a9a28763198658b03600db7b82b1cee7f
erotolo24 <erotolo24@gmail.com>
2023-10-03 13:05:01 -0400

    added basic javascript

---
+++ script.js
@@ -0,0 +1,16 @@
+let preVal = ''
+let newVal = ''
+let resultVal = ''
+let mathOperator = ''
+// stores if decimal was clicked
+//only allow one decimal per value
+let decimalClicked = false
+// hold values we want stored in memory
+let valMemStored = ''
+
+function numButPress(num)
+function mathButPress(operator)
+function equalButPress()
+function clearButPress()
+function copyButPress()
+function pasteButPress()
