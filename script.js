1896d9ff4a8bf8e5803c6243fa76e3ca665d66a0
erotolo24 <erotolo24@gmail.com>
2023-10-03 13:06:01 -0400

    java again

--- script.js
+++ script.js
@@ -8,9 +8,26 @@ let decimalClicked = false
 // hold values we want stored in memory
 let valMemStored = ''
 
-function numButPress(num)
-function mathButPress(operator)
-function equalButPress()
-function clearButPress()
-function copyButPress()
-function pasteButPress()
+function numButPress(num){
+
+}
+
+function mathButPress(operator){
+
+}
+
+function equalButPress(){
+
+}
+
+function clearButPress(){
+
+}
+
+function copyButPress(){
+
+}
+
+function pasteButPress(){
+
+}
